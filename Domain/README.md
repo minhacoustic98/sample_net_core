58d3fe957d61bdc59cc4b8f44ac985094e576470
danny.duong <danny.duong@wealth-dynamix.com>
2022-10-19 17:28:40 +0700

    Initial Project

---
+++ Domain/README.md
@@ -0,0 +1,11 @@
+﻿This layer is called Domain Layer, contains the following items:
+	+ Entities
+	+ Common code
+	+ Exceptions
+	+ Value Objects
+
+Entity
+- Category: 1 category has many products
+- Product: 1 product belong to one category, an only one supplier, has many order
+- Order has many product -> order detail
+- Supplier: 1 supplier has many product
---
+++ Persistence/Configurations/RegionConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence.Configurations
+{
+    public class RegionConfiguration : IEntityTypeConfiguration<Region>
+    {
+        public void Configure(EntityTypeBuilder<Region> builder)
+        {
+            builder.HasKey(e => e.RegionId)
+                .IsClustered(false);
+
+            builder.Property(e => e.RegionId)
+                .HasColumnName("RegionID")
+                .ValueGeneratedNever();
+
+            builder.Property(e => e.RegionDescription)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
